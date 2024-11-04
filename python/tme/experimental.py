81c3a1df88ca19a4987271f31049c852f90abe90
zgbkdlm <zgbkdlm@gmail.com>
2024-11-04 09:32:11 +0100

    folx problem in jvp

--- python/tme/experimental.py
+++ python/tme/experimental.py
@@ -3,11 +3,10 @@ Experimental features.
 """
 import jax
 import jax.numpy as jnp
-from jax import jacfwd
-from folx import forward_laplacian
-from typing import Callable, List, Tuple
-
+from jax import jacfwd, jvp
+from folx import forward_laplacian, register_function, wrap_forward_laplacian
 from tme.typings import Array, JArray, FloatScalar
+from typing import Callable, List, Tuple
 
 
 def generator_power_diagonal(phi: Callable[[Array, FloatScalar], JArray],
@@ -16,6 +15,9 @@ def generator_power_diagonal(phi: Callable[[Array, FloatScalar], JArray],
                              order: int = 1) -> List[Callable[[Array, FloatScalar], JArray]]:
     """When the dispersion is a constant diagonal.
     """
+
+    def jac_part(x, t, f):
+        return jvp(f, (x, t), (drift(x, t), 1.))[1]
 
     def lap(x, t, f):
         f_x = lambda x_: f(x_, t)
@@ -28,9 +30,11 @@ def generator_power_diagonal(phi: Callable[[Array, FloatScalar], JArray],
     for _ in range(order):
         def gen_power(x, t, f=gen_power):
             fwd_lap_results = lap(x, t, f)
-            return (jacfwd(f, argnums=1)(x, t)
-                    + fwd_lap_results.jacobian.dense_array @ drift(x, t)
-                    + 0.5 * fwd_lap_results.laplacian * dispersion ** 2)
+            # return (jacfwd(f, argnums=1)(x, t)
+            #         + fwd_lap_results.jacobian.dense_array @ drift(x, t)
+            #         + 0.5 * fwd_lap_results.laplacian * dispersion ** 2)
+            return jac_part(x, t,
+                            f) + 0.5 * fwd_lap_results.laplacian * dispersion ** 2  # This will compute the Jacobian twice
 
         list_of_gen_powers.append(gen_power)
 
